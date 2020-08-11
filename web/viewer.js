448a9d579f32cac966eb7b6df443a08db1a658e4
clearany <clearanyone@gmail.com>
2020-08-11 15:57:52 +0800

    update

--- web/viewer.js
+++ web/viewer.js
@@ -4350,7 +4350,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER
   },
   defaultUrl: {
-    value: 'compressed.tracemonkey-pldi-09.pdf',
+    value: '../resume/resume_lxy.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
